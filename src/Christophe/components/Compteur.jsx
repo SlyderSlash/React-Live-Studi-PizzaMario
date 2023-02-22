33614e36ad1347dc195ab1a40b55d0d86d70a2ad
Christophe THEVENET <christophethevenet2.0@gmail.com>
2023-02-22 18:11:28 +0100

    style badge compteur

--- src/Christophe/components/Compteur.jsx
+++ src/Christophe/components/Compteur.jsx
@@ -1,33 +1,64 @@
 import { useState } from 'react';
-import { Button, Avatar, Badge } from 'antd';
-import logo from '../assets/img/logo.jpg';
+import { Badge, Image } from 'antd';
+import avatar from '../assets/img/avatar.jpg';
+import { LikeOutlined, DislikeOutlined } from '@ant-design/icons';
 
 const Compteur = () => {
-  const [count, setCount] = useState(null);
+  const [countAvatar, setCountAvatar] = useState(null);
+
+  const [visible, setVisible] = useState(false);
 
   const handlePlus = () => {
-    setCount(count + 1);
+    setCountAvatar(countAvatar + 1);
   };
 
   const handleMinus = () => {
-    setCount(count - 1);
+    setCountAvatar(countAvatar - 1);
   };
 
-  console.log(count);
+  // badge vert si positif et rouge si négatif
+  const handleBackground = () => {
+    return countAvatar > 0 ? { backgroundColor: '#52c41a' } : { backgroundColor: '#c4421a' };
+  };
 
   return (
     <div className="compteurBlock">
       <h1>Compteur de like</h1>
-      <Badge count={count}>
-        <Avatar shape="square" src={logo} className="avatar" />
-      </Badge>
-      <div className="buttonBlock">
-        <Button type="primary" className="btn btnSuccess" onClick={handlePlus}>
-          j'aime
-        </Button>
-        <Button type="primary" danger className="btn btnSuccess" onClick={handleMinus}>
-          je n'aime pas
-        </Button>
+      <div className="imgagesBlock">
+        {/* ------- -------- avatar ---------------- */}
+        <div className="imgageAndButton">
+          <Badge count={countAvatar} style={handleBackground()}>
+            {/*  <Avatar shape="square" src={avatar} className="avatar" size={300}/> */}
+            <Image preview={{ visible: false }} width={250} src={avatar} onClick={() => setVisible(true)} />
+            <div style={{ display: 'none' }}>
+              <Image.PreviewGroup preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}>
+                <Image src={avatar} />
+              </Image.PreviewGroup>
+            </div>
+          </Badge>
+          <div className="buttonBlock">
+            <LikeOutlined style={{ color: '#00ff2a' }} onClick={handlePlus} className="btn btn-good" />
+            <DislikeOutlined style={{ color: '#ff0000' }} onClick={handleMinus} className="btn btn-bad" />
+          </div>
+        </div>
+        {/* ------- -------- fin avatar ---------------- */}
+        {/* ------- -------- avatar ---------------- */}
+        <div className="imgageAndButton">
+          <Badge count={countAvatar} style={handleBackground()}>
+            {/*  <Avatar shape="square" src={avatar} className="avatar" size={300}/> */}
+            <Image preview={{ visible: false }} width={250} src={avatar} onClick={() => setVisible(true)} />
+            <div style={{ display: 'none' }}>
+              <Image.PreviewGroup preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}>
+                <Image src={avatar} />
+              </Image.PreviewGroup>
+            </div>
+          </Badge>
+          <div className="buttonBlock">
+            <LikeOutlined style={{ color: '#00ff2a' }} onClick={handlePlus} className="btn btn-good" />
+            <DislikeOutlined style={{ color: '#ff0000' }} onClick={handleMinus} className="btn btn-bad" />
+          </div>
+        </div>
+        {/* ------- -------- fin avatar ---------------- */}
       </div>
     </div>
   );
